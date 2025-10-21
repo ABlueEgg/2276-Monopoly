35286012a668a71e5bf17b5daf7a6a8bdefb952e
shinhovlsrse217 <hovlrs1890@gmail.com>
2025-10-21 16:17:49 -0700

    I have included unique cards. only few colors for now

---
+++ monopoly/Scripts/CardDatabase.gd
@@ -0,0 +1,8 @@
+const CARDS = { 
+	#first value is attack and second value is health
+	#we can modify it with color and value later
+	"DB_ParkPlace": [2,3],
+	"R_KentuckyAve": [1,1],
+	"R_Illunois": [5,7]
+	
+}
